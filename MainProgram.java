0fd7c328aa1dd512cc167c9c159e2e1ea0b6f9a6
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2025-12-01 07:58:56 -0500

    Add files via upload

---
+++ MainProgram.java
@@ -0,0 +1,14 @@
+package javaPack;
+
+public class MainProgram {
+
+	public static void main(String[] args) {
+		ArrayChar Char = new ArrayChar();
+		System.out.println("Welcome to my letter counting words!");
+		System.out.println("This program will print out each"
+				+ "letter of a word n amount of time\n"
+				+ "Enter a number: ");
+		arrchar.AcceptInfo();
+	}
+
+}
