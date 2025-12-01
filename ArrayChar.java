fcc19711246351d96ce2607870d675e9a267f910
Andrei Bayla <147172993+A-R-E-I@users.noreply.github.com>
2025-12-01 18:58:20 -0500

    Add files via upload

---
+++ ArrayChar.java
@@ -0,0 +1,54 @@
+package javaPack;
+
+import java.util.Scanner;
+
+public class ArrayChar {
+	Scanner allinput = new Scanner(System.in);
+	
+	public void ArrayChar()
+	{
+		AcceptInfo();
+	}
+	
+	public void AcceptInfo()
+	{
+		float numletters = allinput.nextFloat();
+		if(numletters % 1 != 0)
+		{
+			System.out.println("Incorrect value has been entered.\n");
+			MainProgram.main(null);
+		}
+		else
+		{
+			allinput.nextLine();
+			System.out.print("Enter a word: ");
+			String wordent = allinput.nextLine();
+			CheckWord(wordent);
+		}
+	}
+	
+	private void CheckWord(String entword)
+	{
+		int wordlen = entword.length();
+		if(wordlen == 0)
+		{
+			MainProgram.main(null);
+		}
+		else
+		{
+			char[] wordarray = new char[wordlen];
+			for(int i=0; i<wordlen; i++)
+			{
+				wordarray[i] = entword.charAt(i);
+				if((int)wordarray[i] <= 96 || (int)wordarray[i] >= 123)
+				{
+					System.out.print(wordarray[i] + " is not a letter!\n");
+				}
+				else
+				{
+					System.out.print(wordarray[i] + " is a letter!\n");
+				}
+			}
+		}
+	}
+}
--- MainProgram.java
+++ MainProgram.java
@@ -3,9 +3,9 @@ package javaPack;
 public class MainProgram {
 
 	public static void main(String[] args) {
-		ArrayChar Char = new ArrayChar();
+		ArrayChar arrchar = new ArrayChar();
 		System.out.println("Welcome to my letter counting words!");
-		System.out.println("This program will print out each"
+		System.out.println("This program will print out each "
 				+ "letter of a word n amount of time\n"
 				+ "Enter a number: ");
 		arrchar.AcceptInfo();
